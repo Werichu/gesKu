dee58e0bd7a457b2f9c515d2b5795eb21980a93a
Werichu <joseangelmagana011@gmail.com>
2026-01-07 15:41:48 -0600

    Utils file

--- utils.h
+++ utils.h
@@ -2,14 +2,15 @@
 #define UTILS_H_INCLUDED
 #include <iostream>
 #include <ctime>
+#include <cstring>
 
 enum {SALIR,FIJAR,COPIAR,MOVER,ACTIVIDAD};
 
-void fechaActual(){
-    std::string fecha;
-    time_t tiempoAhora;
-    tiempoAhora = time(NULL);
-    ctime(&tiempoAhora);
+std::string fechaActual(){
+    time_t ahora = time(0);
+    char* fecha = ctime(&ahora);
+
+    return fecha;
 }
 
 #endif // UTILS_H_INCLUDED
